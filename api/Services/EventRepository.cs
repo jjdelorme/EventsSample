5d5ac7bf5e060371d7cbe59138d06c35a35a9e87
Jason De Lorme <delormej@gmail.com>
2021-09-30 16:52:19 -0400

    Added mongo.

--- api/Services/EventRepository.cs
+++ api/Services/EventRepository.cs
@@ -1,50 +1,47 @@
-using System;
-using System.Linq;
-using System.Collections;
-using System.Collections.Generic;
+using MongoDB.Driver;
 
 namespace EventsSample
 {
     public class EventRespository
     {
-        private readonly List<Event> _events;
+        private readonly IMongoCollection<Event> _events;
         private readonly ILogger<EventRespository> _log;
         private readonly PublisherService _publisher;
 
-        public EventRespository(ILogger<EventRespository> log, 
+        public EventRespository(IConfiguration config,
+            ILogger<EventRespository> log, 
             PublisherService publisher)
         {
             _log = log;
             _publisher = publisher;
 
-            _events = new List<Event>();
-            _events.Add(new Event() {
-                Id = "0",
-                Date = "3/16/2021",
-                Type = "Manufacturing",
-                Product = "Blue Shoes",
-                Description = "Laces attached"
-            });
-            _events.Add(new Event() {
-                Id = "1",
-                Date = "3/18/2021",
-                Type = "Testing",
-                Product = "Blue Shoes",
-                Description = "Ran up, down hallway"
-            });
+            var connectionString = config["ConnectionString"];
+            var databaseName = config["DatabaseName"];
+            var collectionName = config["CollectionName"];
+
+            if (connectionString == null || databaseName == null ||
+                collectionName == null)
+            {
+                throw new ApplicationException("You must configure ConnectionString, DatabaseName, CollectionName");
+            }
+
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+            _events = database.GetCollection<Event>(collectionName);
         }
 
         public IEnumerable<Event> Get() =>
-            _events;
+            _events.Find(book => true).ToList();
 
         public Event Get(string id) =>
-            _events.Find(e => e.Id == id);
+            _events.Find<Event>(e => e.Id == id).FirstOrDefault();
 
         public async Task<Event> CreateAsync(Event eventItem)
         {
-            // _events.InsertOne(Event);
-            _events.Add(eventItem);
+            _events.InsertOne(eventItem);
+            
             _log.LogInformation($"Created event id:{eventItem.Id}");
+            
             await _publisher.PublishAsync(eventItem);
 
             return eventItem;
