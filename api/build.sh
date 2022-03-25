2178fc5620b3575a7599f1995e54e39a5778a7cb
Jason De Lorme <jasondel@google.com>
2022-03-25 13:17:58 +0000

    Ensuring project & region are set.

--- api/build.sh
+++ api/build.sh
@@ -4,6 +4,19 @@ set -e
 PROJECT_ID=$(gcloud config list --format 'value(core.project)')
 REGION=$(gcloud config list --format 'value(run.region)')
 SUFFIX=$(git rev-parse --short HEAD)
+
+if [ -z $PROJECT_ID ]
+then
+  echo "ERROR.  Please ensure that your core.project is set with gcloud config."
+  exit
+fi
+
+if [ -z $REGION ] 
+then
+  echo "ERROR.  Please ensure that your run/region, for example: gcloud config set run/region us-central1"
+  exit
+fi
+
 IMAGE="$REGION-docker.pkg.dev/$PROJECT_ID/eventssample/eventssample:$SUFFIX"
 
 pushd $PWD
