3aa0b23e1d9ff9ba3c62d77453d0c06bbf46038e
Jason De Lorme <delormej@gmail.com>
2022-06-09 08:06:37 -0400

    Using new prerelease Google SDK logger.

--- api/Program.cs
+++ api/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 if (builder.Environment.IsProduction()) 
 {
     await GoogleMetadata.SetConfigAsync(builder.Configuration);
-    builder.Logging.AddGoogleFormatLogger();
+    builder.Logging.AddGoogleCloudConsole();
 }
 
 // Services
